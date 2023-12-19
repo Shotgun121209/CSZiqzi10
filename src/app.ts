46f7ca885197595b88fe1c25cf3c6373d15d709c
Shotgun121209 <jkil32354@gmail.com>
2023-12-19 22:36:07 +0900

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
+import { resizeImg } from "./helper";
+import cors from "cors";
 
 const app = express();
+const upload = multer();
+const corsOptions = {
+  origin: "*",
+};
+
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 app.get("/hello", (req, res) => {
   //Whatever things you want to do
@@ -11,9 +20,21 @@ app.get("/hello", (req, res) => {
   });
 });
 
-app.post("/image-down", (req, res) => {
+app.post("/resize", upload.single("image"), async (req, res) => {
   //1. Get file
+  const originalFile = req.file;
   //2. Call functino (imgDownScale)
+  if (originalFile?.mimetype.startsWith("image/")) {
+    //Image Down Scale Process
+    const resizedBuff = await resizeImg(originalFile.buffer);
+
+    return res.type("image/jpeg").send(resizedBuff);
+  } else {
+    console.log("Wrong File Format");
+    return res
+      .status(400)
+      .send("Wrong File Type, only image files. (JPG, JPEG, WEBP, PNG, etc)");
+  }
   //3. Send back zip file
 });
 
